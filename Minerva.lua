801b2fd5f1a44dd54d7f0a56f6b4885ae7ce241f
IlseElysium <ilse.elysium@gmail.com>
2017-07-03 23:57:59 +0100

    Reload slash command

--- Minerva.lua
+++ Minerva.lua
@@ -1,3 +1,10 @@
 -- https://www.townlong-yak.com/framexml/1.12.1/ChatFrame.lua#1784
 ChatFrameEditBox.chatType = "GUILD";
 ChatFrameEditBox.stickyType = "GUILD";
+
+-- https://www.townlong-yak.com/framexml/1.12.1/GlobalStrings.lua#3486
+SLASH_RL1 = "/rl";
+-- https://www.townlong-yak.com/framexml/1.12.1/ChatFrame.lua#669
+SlashCmdList["RL"] = function()
+  ReloadUI();
+end
