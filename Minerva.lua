9bb430680c0567b28eae5b28ec2e1804032415d8
IlseElysium <ilse.elysium@gmail.com>
2017-07-04 23:50:49 +0100

    Right-click goname option

--- Minerva.lua
+++ Minerva.lua
@@ -3,7 +3,13 @@ ChatFrameEditBox.chatType = "GUILD";
 ChatFrameEditBox.stickyType = "GUILD";
 
 -- https://www.townlong-yak.com/framexml/1.12.1/GlobalStrings.lua
+GONAME = "Goname";
 MSG_GOLD = "Gold sellers thrive on compromised accounts and our tools for detecting gold buying and tracing gold and items are better than their techniques for preventing buyers from being banned. Help us thwart gold selling by enabling two-factor authentication on your account and by not supporting gold buying. Friends don't let friends buy gold.";
+
+-- https://www.townlong-yak.com/framexml/1.12.1/UnitPopup.lua#12
+UnitPopupButtons["GONAME"] = { text = TEXT(GONAME), dist = 0 };
+-- https://www.townlong-yak.com/framexml/1.12.1/UnitPopup.lua#74
+UnitPopupMenus["FRIEND"] = { "WHISPER", "INVITE", "TARGET", "GUILD_PROMOTE", "GUILD_LEAVE", "GONAME", "CANCEL" };
 
 -- https://www.townlong-yak.com/framexml/1.12.1/GlobalStrings.lua#3486
 SLASH_RL1 = "/rl";
@@ -21,4 +27,13 @@ function ChatFrame_OnEvent(event)
     end
   end
   Original_ChatFrame_OnEvent(event);
+end
+
+-- https://www.townlong-yak.com/framexml/1.12.1/UnitPopup.lua#536
+local Original_UnitPopup_OnClick = UnitPopup_OnClick;
+function UnitPopup_OnClick()
+  if ( this.value == "GONAME" ) then
+    SendChatMessage(".goname "..FriendsDropDown.name);
+  end
+  Original_UnitPopup_OnClick();
 end
