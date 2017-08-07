892c638c923a0bab0a04f8bdde1b19eba3164c0e
IlseElysium <ilse.elysium@gmail.com>
2017-08-07 21:41:03 +0100

    something with online tickets idk

--- Minerva.lua
+++ Minerva.lua
@@ -98,6 +98,12 @@ function ChatFrame_OnEvent(event)
       if ( FILTER_TICKETS ) then
         return;
       end
+    elseif ( arg1 == "Showing list of open tickets whose creator is online." ) then
+      if ( MinervaFrame.filter ) then
+        FILTER_TICKETS = false;
+      end
+      MinervaFrame.filter = true;
+      TICKET_NUMBER = 1;
     elseif ( string.match(arg1, "|cffaaffaaTicket|r:|cffaaccff %d+%.|r |cff00ff00Creator|r:|cff00ccff |cffffffff|Hplayer:%a+|h%[%a+%]|h|r|r |cff00ff00Created|r:|cff00ccff [%d+0dhms]- ago|r.*") and not string.match(arg1, ".*Ticket Message.*") ) then
       local ticketNumber = string.gsub(arg1, "^|cffaaffaaTicket|r:|cffaaccff (%d+).+", "%1");
       local ticketOwner = string.gsub(arg1, "^|cffaaffaaTicket|r:|cffaaccff %d+%.|r |cff00ff00Creator|r:|cff00ccff |cffffffff|Hplayer:(%a+).+", "%1");
